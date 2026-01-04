c585eaa45c3388f4c779cb68006603958db6ce2d
Caleb Dennis <caleb.dennis429@gmail.com>
2026-01-03 17:24:55 -0700

    working dirichlet, working exponential/sinusoid solutions

--- fem1d.py
+++ fem1d.py
@@ -26,11 +26,11 @@ BilinearForm = Callable[[BasisFunc, BasisFunc, float, float], float]
 @dataclass
 class GlobalNode:
     idx: int
-    is_boundry: bool
+    is_boundary: bool
 
     @staticmethod
     def from_ints(ints: list[int]) -> list[GlobalNode]:
-        return [GlobalNode(idx=i, is_boundry=False) for i in ints]
+        return [GlobalNode(idx=i, is_boundary=False) for i in ints]
 
 @dataclass
 class Element1D:
@@ -57,31 +57,32 @@ class Element1D:
     def order(self) -> int:
         return len(self.basis.f)
 
-class BoundryType(enum.Enum):
+class BoundaryType(enum.Enum):
     Homogenous = enum.auto
     Dirichlet = enum.auto
     Mixed = enum.auto
 
 @dataclass
-class Boundry1D:
-    btype: BoundryType
+class Boundary1D:
+    btype: BoundaryType
     vals: list[float]
 
-    # true if boundry adds conditions to the function
+    # true if boundary adds conditions to the function
     def is_func_condition(self) -> bool:
-        return  (self.btype == BoundryType.Dirichlet or self.btype == BoundryType.Homogenous)
+        return  (self.btype == BoundaryType.Dirichlet or self.btype == BoundaryType.Homogenous)
 
-    # true if boundry adds conditions to the functions deriviative
+    # true if boundary adds conditions to the functions deriviative
     def is_deriv_condition(self) -> bool:
-        return (self.btype == BoundryType.Mixed) and len(self.vals) == 3
+        return (self.btype == BoundaryType.Mixed) and len(self.vals) == 3
 
-class BoundryValueProblem1D:
+class BoundaryValueProblem1D:
     M: Matrix[float]
     elements: Sequence[Element1D]
     b: Sequence[float]
     op: BilinearForm
+    boundary: Boundary1D
 
-    def __init__(self, op: BilinearForm, elements: Sequence[Element1D], f: ForcingFunc, boundary: Boundry1D):
+    def __init__(self, op: BilinearForm, elements: Sequence[Element1D], f: ForcingFunc, boundary: Boundary1D):
         self.M = [[0.0 for _ in range(len(elements) + 1)] for _ in range(len(elements) + 1)]
         self.b = [0.0 for _ in range(len(elements) + 1)]
         self.op = op
@@ -90,8 +91,6 @@ class BoundryValueProblem1D:
             ematrix = e.element_matrix(self.op)
             for i in range(e.order()):
                 for j in range(e.order()):
-                    if e.n[i].is_boundry and boundary.is_func_condition():
-                        continue
                     self.M[e.n[i].idx][e.n[j].idx] += ematrix[i][j]
 
             # compute forcing element
@@ -100,20 +99,27 @@ class BoundryValueProblem1D:
                 self.b[e.n[i].idx] += products[i]
 
         # modify `b` to implement general Dirichlet condition
-        if boundary.btype == BoundryType.Dirichlet:
+        if boundary.btype == BoundaryType.Dirichlet:
             ematrix_0_1_0 = self.op(elements[0].basis.f[0], elements[0].basis.f[1], elements[0].x0, elements[0].x1)
             self.b[elements[0].n[1].idx] -= boundary.vals[0] * ematrix_0_1_0
             ematrix_N_0_1 = self.op(elements[-1].basis.f[1], elements[-1].basis.f[0], elements[-1].x0, elements[-1].x1)
             self.b[elements[-1].n[0].idx] -= boundary.vals[1] * ematrix_N_0_1
         # modify `b` and `M` to implement Neumann conditions
-        elif boundary.btype == BoundryType.Mixed:
+        elif boundary.btype == BoundaryType.Mixed:
             self.b[elements[0].n[0].idx] -= boundary.vals[0] * elements[0].basis.f[0][0](0)
             self.b[elements[-1].n[1].idx] += boundary.vals[1] * elements[-1].basis.f[1][0](0)
             self.M[elements[0].n[0].idx][elements[0].n[0].idx] -= boundary.vals[2] * elements[0].basis.f[0][0](0)
             self.M[elements[-1].n[1].idx][elements[-1].n[1].idx] += boundary.vals[2] * elements[-1].basis.f[1][0](0)
 
+        start = 1 if boundary.is_func_condition() else 0
+        end = -1 if boundary.is_func_condition else None
+        # Dirichlet condition sets first and last equations in `M` to 0
+        if boundary.is_func_condition():
+            self.M = [row[start:end] for row in self.M[start:end]]
+            self.b = self.b[start:end]
+
         self.elements = elements
-        self.b = [-3.0] + [0.0 for _ in range(N - 1)] + [3.0]
+        self.boundary = boundary
 
     # solve for coefficients
     def solve(self) -> Sequence[float]:
@@ -121,9 +127,12 @@ class BoundryValueProblem1D:
 
     # construct solution function from coefficients
     def result(self, x: float, u: Sequence[float]) -> float:
+        if self.boundary.is_func_condition():
+            u = [self.boundary.vals[0]] + list(u) + [self.boundary.vals[1]]
         for e in elements:
             if x <= e.x1 and x >= e.x0:
                 return sum([f[0](x) * u[n.idx] for (f, n) in zip(e.basis.f, e.n)])
+
         raise ValueError
 
 
@@ -134,12 +143,12 @@ if __name__ == "__main__":
     k = 1
     points = np.linspace(0, L, N + 1)
     def a(u: BasisFunc, v: BasisFunc, a: float, b: float) -> float:
-        return quad(lambda x: u[1](x)*v[1](x) + k*k*u[0](x)*v[0](x), a, b)[0]
+        return quad(lambda x: u[1](x)*v[1](x) - k*k*u[0](x)*v[0](x), a, b)[0]
     elements = [Element1D(x0, x1, GlobalNode.from_ints([i, i+1]), Basis1D.linear_basis(x0, x1)) for (i, (x0, x1)) in enumerate(zip(points[0:-1], points[1:]))]
-    elements[0].n[0].is_boundry = True
-    elements[-1].n[1].is_boundry = True
+    elements[0].n[0].is_boundary = True
+    elements[-1].n[1].is_boundary = True
 
-    problem = BoundryValueProblem1D(a, elements, lambda x: 0.0, Boundry1D(BoundryType.Dirichlet, [1,0]))
+    problem = BoundaryValueProblem1D(a, elements, lambda x: 0.0, Boundary1D(BoundaryType.Dirichlet, [1, 1]))
     print(np.array(problem.M))
 
     u = problem.solve()
